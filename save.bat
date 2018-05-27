b2dd17028661177f11b5c0c0474681eb9c269a68
ymy14 <1294366899@qq.com>
2018-05-27 16:34:44 +0800

    update

--- save.bat
+++ save.bat
@@ -1,5 +1,6 @@
 git add .
 git commit -m "update"
+git push origin master
 @echo ""
 @echo Save!!! Good Job!!!
 pause
--- test.bat
+++
@@ -1,4 +0,0 @@
-git add .
-git commit -m "First cm"
-git push -u origin master
-pause
